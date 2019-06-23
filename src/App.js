d0c9f3a1603e259158528d7552457aa011cf897c
tuantv <tuanhanhphuc123@gmail.com>
2019-06-23 21:46:00 +0700

    add card animation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useGesture } from 'react-use-gesture';
+import { useSprings, a } from 'react-spring';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Cards() {
+  // Set up 3 springs, each carries x/y coords and z-rotation
+  // We add up mass and friction to make the later springs heavier
+  const [springs, set] = useSprings(3, i => ({
+    xyz: [0, -i * 30, 0],
+    config: { mass: 1 + i * 2, tension: 700 - i * 100, friction: 30 + i * 20 }
+  }))
+  // Prepare a function that casts a spring into a css-transform
+  const transform = i => springs[i].xyz.interpolate((x, y, z) => `translate3d(${x}px,${y}px,0) scale(${1 - i * 0.05}) rotateZ(${z}deg)`)
+  // Tie a drag gesture to the view, this gives us local coordinates and pointer-down state
+  // This gesture updates the springs without re-rendering the view
+  const bind = useGesture(({ local: [x, y], down }) => set(i => ({ xyz: [x + down * i * 20, y - i * 30 - down * i * 120, down * i * 10] })))
+  // Render 3 divs and wire up the springss
+  return new Array(3).fill().map((_, index) => <a.div key={index} {...bind()} style={{ transform: transform(3 - index - 1) }} />)
 }
 
-export default App;
+export default Cards;
